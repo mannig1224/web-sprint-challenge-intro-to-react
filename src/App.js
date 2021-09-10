61101eb0237d730bc1d53a334d2bd9a93d50bba1
ManniG24 <egatica51@gmail.com>
2021-09-10 16:51:08 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import {url} from './config'
+import Character from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characters, setCharacters] = useState();
+  const [currentPageUrl, setcurrentPageUrl] = useState(url);
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios.get(currentPageUrl)
+      .then(results => {
+        console.log(results);
+        setCharacters(results.data);
+        setNextPageUrl()
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [currentPageUrl])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {(!characters) ? <h3>Loading...</h3> :<Character characters={characters} />}
     </div>
   );
 }
